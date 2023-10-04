8c7b241de23b83d56df69fc36a1b1d7e609a10ff
Yannick Witzky <ywitzky@students.uni-mainz.de>
2023-10-04 13:12:36 +0200

    successfull C binding to library file

--- GSD_Julia/C_Binding_test.jl
+++ GSD_Julia/C_Binding_test.jl
@@ -1,4 +1,13 @@
+
+
 using CBinding, Libdl#, Libc
+import Base: unsafe_convert
+#import Base.unsafe_convert
+
+unsafe_convert(::Type{Cptr{Nothing}}, _::Int64) =Cptr{Nothing}() ### assumption that this doesnt cause issues
+#unsafe_convert(::Type{Cptr{Nothing}}, _::Int64) = 0 ### assumption that this doesnt cause issues
+
+#unsafe_convert(::Type{Cptr{Cconst{var"c\"struct gsd_index_entry\""}}}, ::String)
 
 libpath= Libdl.find_library("libgsd.so", vcat(Base.DL_LOAD_PATH, ["/uni-mainz.de/homes/ywitzky/phdscripts/GSD/GSD_Julia/gsd/"]) )
 
@@ -20,7 +29,9 @@ c"""
 #include "/uni-mainz.de/homes/ywitzky/phdscripts/GSD/GSD_Julia/gsd/gsd.h"
 """j
 
+unsafe_convert(::Type{Cptr{Cconst{var"c\"struct gsd_index_entry\""}}}, a::String) = begin println(a * "test"); return Cptr{Cconst{var"c\"struct gsd_index_entry\""}}(pointer_from_objref(Ref(a))); end ### only for line 6something in gsd.c where he uses char* to define pointer position
 
+unsafe_convert(::Type{Cptr{var"c\"struct gsd_handle\""}}, a::var"(c\"struct gsd_handle\")") = Cptr{var"c\"struct gsd_handle\""}(pointer_from_objref(Ref(a)))
 
 header = c"struct gsd_header"(magic = 1)
 
@@ -35,11 +46,56 @@ bla = [gsd_handle_var]
 #println(gsd_handle_var)
 #ptr  = c"struct gsd_handle * " gsd_handle_var
 #retval = gsd_read_chunk(bla, 0,"configuration/dimensions")
+#bla = (gsd_handle_var)
+#println(typeof(bla))
+# pointer_from_objref(Ref(gsd_handle_var))
+
+
+#convert(::Type{Cptr{typof(gsd_handle_var)}}, x) = Ref(x)
+
+
 retval = gsd_read_chunk(pointer_from_objref(Ref(gsd_handle_var)), 0,"configuration/dimensions")
+
 #c"gsd_read_chunk"(gsd_handle2, Cint(0),"configuration/dimensions")
 
 version =gsd_make_version(UInt32(1),UInt32(4))
 retval = gsd_create("./test_here.gsd", "hoomd", "hoomd", version)
-println(retval)
-println(gsd_handle_var)
+#println(retval)
+#println(gsd_handle_var)
 
+obj=gsd_handle()
+flag = GSD_OPEN_READONLY
+
+file = "/localscratch/HPS_DATA/HPS-Janka/RS31a/run1/MDhnRNPA1seqRS31a_M250_NP1_T300_Box40_40_40_s1000000000.gsd"
+#println(gsd_create_and_open(obj, file, "application", "schema", UInt32(1), flag, Int32(1)))
+
+#println(flag)
+#println(gsd_close(obj))
+retval =  gsd_open(obj, file, flag)
+#println(typeof(new_handle))
+#obj=gsd_handle()
+
+
+#from fl.pyx : read_chunk
+index_entry = gsd_index_entry()
+index_entry = gsd_find_chunk(pointer_from_objref(Ref(obj)),1,"configuration/dimensions" )#dimensons
+println(obj.fd)
+println("index entry")
+println(index_entry)
+println(index_entry.type)
+println(GSD_TYPE_UINT8)
+println(UInt8(index_entry.type)   , " ", UInt8(c"GSD_TYPE_UINT8"))
+println(UInt8(index_entry.type) >>> 4 == UInt8(c"GSD_TYPE_UINT8"))
+
+println(index_entry.N, "  ", index_entry.M)
+println(UInt64(index_entry.N))
+println(UInt64((index_entry.M)))
+println("asdh")
+println(gsd_sizeof_type(c"enum gsd_type"(4)))
+
+data=zeros(Float64 , (UInt64(index_entry.N), UInt32(index_entry.M)))
+data_ptr = pointer_from_objref(Ref(data))
+retval = gsd_read_chunk(pointer_from_objref(Ref(obj)), data_ptr, pointer_from_objref(Ref(index_entry)))
+
+println(retval)
+println(data)
