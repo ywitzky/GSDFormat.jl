eb0eefff167c4d62f637bf6c627b7008ec74d63e
Yannick Witzky <ywitzky@students.uni-mainz.de>
2024-02-12 22:58:41 +0100

    translation of _read_Frame to julia. not tested, lacks python specifics

--- GSD_Julia/Structs.jl
+++ GSD_Julia/Structs.jl
@@ -98,141 +98,139 @@ function _read_frame(traj::HOOMDTrajectory{Integer}, idx::Integer):
     #logger.debug('reading frame ' + str(idx) + ' from: ' + str(self.file))
 
     if isnothing(traj.initial_frame) && idx != 0:
-        self._read_frame(0)
+        read_frame(traj, 0)
 
     frame = Frame()
     # read configuration first
-    if self.file.chunk_exists(frame=idx, name='configuration/step'):
-        step_arr = self.file.read_chunk(frame=idx,
-                                        name='configuration/step')
+    if chunk_exists(traj.file, frame=idx, name="configuration/step"):
+        step_arr = read_chunk(traj.file , frame=idx, name="configuration/step")
         frame.configuration.step = step_arr[0]
-    else:
-        if self._initial_frame is not None:
-            frame.configuration.step = \
-                self._initial_frame.configuration.step
-        else:
-            frame.configuration.step = \
-                frame.configuration._default_value['step']
+    else
+        if ~isnothing(traj.initial_frame)
+            frame.configuration.step = traj.initial_frame.configuration.step
+        else
+            frame.configuration.step = frame.configuration._default_value["step"]
+        end
+    end
 
-    if self.file.chunk_exists(frame=idx, name='configuration/dimensions'):
-        dimensions_arr = self.file.read_chunk(
-            frame=idx, name='configuration/dimensions')
+    if chunk_exists(traj.file, frame=idx, name="configuration/dimensions")
+        dimensions_arr = read_chunk(traj,frame=idx, name="configuration/dimensions")
         frame.configuration.dimensions = dimensions_arr[0]
-    else:
-        if self._initial_frame is not None:
-            frame.configuration.dimensions = \
-                self._initial_frame.configuration.dimensions
-        else:
-            frame.configuration.dimensions = \
-                frame.configuration._default_value['dimensions']
+    else
+        if ~isnothing(traj.initial_frame)
+            frame.configuration.dimensions = traj.initial_frame.configuration.dimensions
+        else
+            frame.configuration.dimensions = frame.configuration._default_value["dimensions"]
+        end
+    end
 
-    if self.file.chunk_exists(frame=idx, name='configuration/box'):
-        frame.configuration.box = self.file.read_chunk(
-            frame=idx, name='configuration/box')
-    else:
-        if self._initial_frame is not None:
-            frame.configuration.box = self._initial_frame.configuration.box
-        else:
-            frame.configuration.box = \
-                frame.configuration._default_value['box']
+    if chunk_exists(traj.fileframe=idx, name="configuration/box")
+        frame.configuration.box = read_chunk(traj.file,frame=idx, name="configuration/box")
+    else
+        if ~isnothing(traj.initial_frame)
+            frame.configuration.box = traj.initial_frame.configuration.box
+        else
+            frame.configuration.box = frame.configuration._default_value["box"]
+        end
+    end
 
     # then read all groups that have N, types, etc...
-    for path in [
-            'particles',
-            'bonds',
-            'angles',
-            'dihedrals',
-            'impropers',
-            'constraints',
-            'pairs',
-    ]:
-        container = getattr(frame, path)
-        if self._initial_frame is not None:
-            initial_frame_container = getattr(self._initial_frame, path)
+    for path in ["particles","bonds","angles","dihedrals","impropers","constraints","pairs"]
+
+        container = getproperty(frame, Symbol(path))
+        if ~isnothing(traj_initial_frame)
+            initial_frame_container = getproperty(traj.initial_frame, Symbol(path))
+        end
 
         container.N = 0
-        if self.file.chunk_exists(frame=idx, name=path + '/N'):
-            N_arr = self.file.read_chunk(frame=idx, name=path + '/N')
+        if chunk_exists(traj.file, frame=idx, name=path * '/N')
+            N_arr = read_chunk(traj.file, frame=idx, name=path * '/N')
             container.N = N_arr[0]
-        else:
-            if self._initial_frame is not None:
+        else
+            if ~isnothing(traj.initial_frame)
                 container.N = initial_frame_container.N
+            end
+        end
 
         # type names
         if 'types' in container._default_value:
-            if self.file.chunk_exists(frame=idx, name=path + '/types'):
-                tmp = self.file.read_chunk(frame=idx, name=path + '/types')
-                tmp = tmp.view(dtype=numpy.dtype((bytes, tmp.shape[1])))
-                tmp = tmp.reshape([tmp.shape[0]])
+            if chunk_exists(traj.file, frame=idx, name=path * "/types")
+                tmp = read_chunk(traj.file, frame=idx, name=path * "/types")
+                #tmp = tmp.view(dtype=numpy.dtype((bytes, tmp.shape[1])))
+                #tmp = tmp.reshape([tmp.shape[0]])
                 container.types = list(a.decode('UTF-8') for a in tmp)
-            else:
-                if self._initial_frame is not None:
+            else
+                if ~isnothing(traj.initial_frame)
                     container.types = initial_frame_container.types
-                else:
-                    container.types = container._default_value['types']
+                else
+                    container.types = container._default_value["types"]
+                end
+            end
+        end
 
         # type shapes
-        if ('type_shapes' in container._default_value
-                and path == 'particles'):
-            if self.file.chunk_exists(frame=idx,
-                                    name=path + '/type_shapes'):
-                tmp = self.file.read_chunk(frame=idx,
-                                        name=path + '/type_shapes')
-                tmp = tmp.view(dtype=numpy.dtype((bytes, tmp.shape[1])))
-                tmp = tmp.reshape([tmp.shape[0]])
-                container.type_shapes = \
-                    list(json.loads(json_string.decode('UTF-8'))
-                        for json_string in tmp)
-            else:
-                if self._initial_frame is not None:
-                    container.type_shapes = \
-                        initial_frame_container.type_shapes
-                else:
-                    container.type_shapes = \
-                        container._default_value['type_shapes']
+        if ("type_shapes" in container._default_value && path == "particles")
+            if chunk_exists(traj.file, frame=idx, name=path * "/type_shapes")
+                tmp = read_chunk(traj.file, frame=idx, name=path * "/type_shapes")
+                #tmp = tmp.view(dtype=numpy.dtype((bytes, tmp.shape[1])))
+                #tmp = tmp.reshape([tmp.shape[0]])
+                #container.type_shapes =  list(json.loads(json_string.decode('UTF-8')     for json_string in tmp)
+            else
+                if ~isnothing(traj.initial_frame)
+                    container.type_shapes = initial_frame_container.type_shapes
+                else
+                    container.type_shapes = container._default_value["type_shapes"]
+                end
+            end
+        end
 
-        for name in container._default_value:
-            if name in ('N', 'types', 'type_shapes'):
-                continue
+        for name in container._default_value
+            if name in ("N", "types", "type_shapes") continue end
 
             # per particle/bond quantities
-            if self.file.chunk_exists(frame=idx, name=path + '/' + name):
-                container.__dict__[name] = self.file.read_chunk(
+            if chunk_exists(traj.file, frame=idx, name=path * '/' + name)
+                container.__dict__[name] = read_chunk(traj.file
                     frame=idx, name=path + '/' + name)
-            else:
-                if (self._initial_frame is not None
-                        and initial_frame_container.N == container.N):
+            else
+                if isnothing(traj.initial_frame) &&  initial_frame_container.N == container.N
                     # read default from initial frame
                     container.__dict__[name] = \
                         initial_frame_container.__dict__[name]
-                else:
+                else
                     # initialize from default value
-                    tmp = numpy.array([container._default_value[name]])
+                    #tmp = numpy.array([container._default_value[name]])
+                    tmp = zeros([container._default_value[name]])
+
                     s = list(tmp.shape)
                     s[0] = container.N
-                    container.__dict__[name] = numpy.empty(shape=s,
-                                                        dtype=tmp.dtype)
+                    container.__dict__[name] = numpy.empty(shape=s, dtype=tmp.dtype)
                     container.__dict__[name][:] = tmp
-
-                container.__dict__[name].flags.writeable = False
+                end
+                container.__dict__[name].flags.writeable = false
+            end
+        end
 
     # read state data
-    for state in frame._valid_state:
-        if self.file.chunk_exists(frame=idx, name='state/' + state):
-            frame.state[state] = self.file.read_chunk(frame=idx,
-                                                    name='state/' + state)
+    for state in frame._valid_state
+        if chunk_exists(traj.file, frame=idx, name="state/" * state)
+            frame.state[state] = read_chunk(traj.file, frame=idx, name="state/" * state)
+        end
+    end
 
     # read log data
-    logged_data_names = self.file.find_matching_chunk_names('log/')
-    for log in logged_data_names:
-        if self.file.chunk_exists(frame=idx, name=log):
+    logged_data_names = find_matching_chunk_names(traj.file, "log/")
+    for log in logged_data_names
+        if chunk_exists(traj.file, frame=idx, name=log)
             frame.log[log[4:]] = self.file.read_chunk(frame=idx, name=log)
-        else:
-            if self._initial_frame is not None:
+        else
+            if ~isnothing(traj.initial_frame)
                 frame.log[log[4:]] = self._initial_frame.log[log[4:]]
+            end
+        end
+    end
 
     # store initial frame
-    if self._initial_frame is None and idx == 0:
+    if ~isnothing(traj.initial_frame) && idx == 0:
         self._initial_frame = frame
 
     return frame
