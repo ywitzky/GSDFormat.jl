17109d8c355fb9de5fd4584d9f8749063dd515ec
Yannick Witzky <ywitzky@students.uni-mainz.de>
2024-02-21 10:22:11 +0100

    first complete draft of _read_frame. untested

--- GSD_Julia/HOOMDTrajectory.jl
+++ GSD_Julia/HOOMDTrajectory.jl
@@ -1,5 +1,8 @@
 ### copies class definitions from hoomd.py
 
+include("./Structs.jl")
+
+using JSON
 import Base: size, getindex
 
 mutable struct HOOMDTrajectory{I<:Integer}
@@ -33,7 +36,7 @@ function init_HOOMDTrajectory(file::GSDFILE{I}) where {I<:Integer}
     if (version < (2, 0) && version >= (1, 0))
         valid = true
     end
-    if ~valid
+    if !valid
         throw(InitError("Incompatible hoomd schema version "* str(version) * " in: " * str(self.file)))
     end
 
@@ -103,7 +106,7 @@ function _read_frame(traj::HOOMDTrajectory{Integer}, idx::Integer)
 
     frame = Frame()
     # read configuration first
-    if chunk_exists(traj.file, frame=idx, name="configuration/step"):
+    if chunk_exists(traj.file, frame=idx, name="configuration/step")
         step_arr = read_chunk(traj.file , frame=idx, name="configuration/step")
         frame.configuration.step = step_arr[0]
     else
@@ -144,8 +147,8 @@ function _read_frame(traj::HOOMDTrajectory{Integer}, idx::Integer)
         end
 
         container.N = 0
-        if chunk_exists(traj.file, frame=idx, name=path * '/N')
-            N_arr = read_chunk(traj.file, frame=idx, name=path * '/N')
+        if chunk_exists(traj.file, frame=idx, name=path * "/N")
+            N_arr = read_chunk(traj.file, frame=idx, name=path * "/N")
             container.N = N_arr[0]
         else
             if ~isnothing(traj.initial_frame)
@@ -154,17 +157,14 @@ function _read_frame(traj::HOOMDTrajectory{Integer}, idx::Integer)
         end
 
         # type names
-        if 'types' in container._default_value:
+        if typeof(container) in [ParticleData,BondData{Any} ] 
             if chunk_exists(traj.file, frame=idx, name=path * "/types")
-                tmp = read_chunk(traj.file, frame=idx, name=path * "/types")
-                #tmp = tmp.view(dtype=numpy.dtype((bytes, tmp.shape[1])))
-                #tmp = tmp.reshape([tmp.shape[0]])
-                container.types = list(a.decode('UTF-8') for a in tmp)
+                container.types = read_chunk(traj.file, frame=idx, name=path * "/types")
             else
                 if ~isnothing(traj.initial_frame)
                     container.types = initial_frame_container.types
                 else
-                    container.types = container._default_value["types"]
+                    container.types = get_default("types", container)
                 end
             end
         end
@@ -172,49 +172,36 @@ function _read_frame(traj::HOOMDTrajectory{Integer}, idx::Integer)
         # type shapes
         if ("type_shapes" in container._default_value && path == "particles")
             if chunk_exists(traj.file, frame=idx, name=path * "/type_shapes")
-                tmp = read_chunk(traj.file, frame=idx, name=path * "/type_shapes")
-                #tmp = tmp.view(dtype=numpy.dtype((bytes, tmp.shape[1])))
-                #tmp = tmp.reshape([tmp.shape[0]])
-                #container.type_shapes =  list(json.loads(json_string.decode('UTF-8')     for json_string in tmp)
+                container.type_shapes = JSON.parse.(read_chunk(traj.file, frame=idx, name=path * "/type_shapes"))
             else
                 if ~isnothing(traj.initial_frame)
                     container.type_shapes = initial_frame_container.type_shapes
                 else
-                    container.type_shapes = container._default_value["type_shapes"]
+                    container.type_shapes = get_default("type_shapes", container)
                 end
             end
         end
 
-        for name in container._default_value
-            if name in ("N", "types", "type_shapes") continue end
-
+        for name in get_container_names(container)
             # per particle/bond quantities
-            if chunk_exists(traj.file, frame=idx, name=path * '/' + name)
-                container.__dict__[name] = read_chunk(traj.file
-                    frame=idx, name=path + '/' + name)
+            if chunk_exists(traj.file, frame=idx, name="$path/$name")
+                setproperty!(container, name, read_chunk(traj.file,frame=idx, name="$path/$name"))
             else
                 if isnothing(traj.initial_frame) &&  initial_frame_container.N == container.N
                     # read default from initial frame
-                    container.__dict__[name] = \
-                        initial_frame_container.__dict__[name]
+                    setproperty!(container, name, getproperty(initial_frame_container, name))
                 else
                     # initialize from default value
-                    #tmp = numpy.array([container._default_value[name]])
-                    tmp = zeros([container._default_value[name]])
-
-                    s = list(tmp.shape)
-                    s[0] = container.N
-                    container.__dict__[name] = numpy.empty(shape=s, dtype=tmp.dtype)
-                    container.__dict__[name][:] = tmp
+                    setproperty!(container, name, copy(get_default("$name", container)) )
                 end
-                container.__dict__[name].flags.writeable = false
+                getproperty(container, name).flags.writable=false
             end
         end
 
     # read state data
-    for state in frame._valid_state
-        if chunk_exists(traj.file, frame=idx, name="state/" * state)
-            frame.state[state] = read_chunk(traj.file, frame=idx, name="state/" * state)
+    for state in frame.valid_state
+        if chunk_exists(traj.file, frame=idx, name="state/$state")
+            frame.state[state] = read_chunk(traj.file, frame=idx, name="state/$state")
         end
     end
 
@@ -222,10 +209,10 @@ function _read_frame(traj::HOOMDTrajectory{Integer}, idx::Integer)
     logged_data_names = find_matching_chunk_names(traj.file, "log/")
     for log in logged_data_names
         if chunk_exists(traj.file, frame=idx, name=log)
-            frame.log[log[4:]] = self.file.read_chunk(frame=idx, name=log)
+            frame.log[log[4:end]] = self.file.read_chunk(frame=idx, name=log)
         else
             if ~isnothing(traj.initial_frame)
-                frame.log[log[4:]] = self._initial_frame.log[log[4:]]
+                frame.log[log[4:end]] = self._initial_frame.log[log[4:end]]
             end
         end
     end
