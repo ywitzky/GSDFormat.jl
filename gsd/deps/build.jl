d51666e33396f06824763767793139d164be7c51
Yannick Witzky <ywitzky@students.uni-mainz.de>
2024-03-02 17:04:38 +0100

    implemented auto build of gsd and libgsd.o

---
+++ gsd/deps/build.jl
@@ -0,0 +1,25 @@
+using LibGit2, CMake
+
+println(pathof(LibGit2))
+#folder_dir=strip(pathof(GSD), "src/LibGit2.jl")
+#cpp_dir = "$(folder_dir)gsd_cpp/"
+
+cpp_dir = "/uni-mainz.de/homes/ywitzky/Code_Projects/gsd/gsd/gsd_cpp"
+println(cpp_dir)
+
+if isdir(cpp_dir)
+    println("GSD: Delete $cpp_dir if github should be checked for updates.")
+else
+    println("GSD: https://github.com/glotzerlab/gsd.git.")
+    LibGit2.clone("https://github.com/glotzerlab/gsd.git", cpp_dir)
+end
+
+println("GSD: Compile libgsd.so")
+cd("$cpp_dir/gsd/")
+run(`$cmake CMakeLists.txt`)
+run(`make gsd.o`)
+run(`gcc -g -shared -o libgsd.so ./CMakeFiles/fl.dir/gsd.o`)
+
+include("../src/libgsd_wrapper.jl")
+
+
